5ec3b17390d938014e0baeb2e6b90afbad510967
sesas <gabreyla@gmail.com>
2012-03-26 17:24:18 -0700

    added a test folder, dirName input and file walker creation.
    Signed-off-by: sesas <gabreyla@gmail.com>

--- fileWalker.py
+++ fileWalker.py
@@ -1,3 +1,40 @@
 import os, sys
 import UltraFastDuplicateFilesFinder as ff
 
+testPath = os.path.join( os.path.curdir )
+
+
+
+
+
+
+
+def getDirName(dirName=None):
+    if dirName:
+        if os.path.isdir( os.path.normpath( dirName )):
+            return dirName
+    if sys.argv[1:]:
+        out = sys.argv[1]
+        if os.path.isdir( os.path.normpath( out )):
+            return out
+    
+    while 1:
+        inp = input("which folder would you like to find the duplicates in?\n")
+        if not inp: # mainly for debugging
+            global testPath
+            inp = testPath
+            break
+        if os.path.isdir( os.path.normpath( inp )):
+            break
+    return inp
+    
+
+def main(dirName=None):
+    root = getDirName(dirName)
+    walker = os.walk(root)
+
+    for j in walker:
+        print(j)
+
+if __name__ == '__main__':
+    main()
